3601c588942a01d50fab54bd42c28d389c17ea2d
seunkoko <seunowonikoko@gmail.com>
2021-08-01 18:14:03 +0300

    Make api calls to get markers

--- src/components/Layout/MainContent/index.js
+++ src/components/Layout/MainContent/index.js
@@ -1,52 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { useParams } from 'react-router';
 import { withRouter } from 'react-router-dom';
+import { fetchApi } from '../../../utils/fetchApi';
 import { getToken } from '../../../utils/helper';
 
 import './MainContent.scss';
 
-const defaultMarkers = [{
-        modified_at: "2021-08-01T11:49:48.646096",
-        is_active: true,
-        created_at: "2021-08-01T11:36:03.883489",
-        name: "updated seunkoko1 Pin",
-        latLng: [
-            4.0,
-            6.0
-        ],
-        user: {
-            "username": "seunkoko1",
-            id: "-Mg0AaFSPQ8DRTVzEp8H"
-        },
-        id: "-Mg0UyFeajwiIvaju8hA",
-        shared: false,
-        user_id: "-Mg0AaFSPQ8DRTVzEp8H"
-    },
-    {
-        modified_at: "2021-08-01T11:49:48.646096",
-        is_active: true,
-        created_at: "2021-08-01T11:36:03.883489",
-        name: "updated seunkoko1 Pin",
-        latLng: [
-            4.0,
-            6.0
-        ],
-        user: {
-            "username": "seunkoko2",
-            id: "-Mg0AaFSPQ8DRTVzEp8H"
-        },
-        id: "-Mg0UyFeajwiIvaju8hA",
-        shared: false,
-        user_id: "-Mg0AaFSPQ8DRTVzEp8H"
-    }
-]
 
 const MainContent = ({ history, handleToggleSidebar }) => {
-	const [markers, setMarkers] = useState(defaultMarkers);
+	const [markers, setMarkers] = useState([]);
 
     let { id } = useParams();
     if (!getToken()) history.push('/login')
+
+    useEffect(() => {
+        const asyncFetchApi = async () => await fetchApi('user_info', 'GET')
+
+        asyncFetchApi()
+        .then((data) => {
+            if (data.status === 'success') {
+                if (id === 'shared') setMarkers(data.data.user.my_shares)
+                else if (id === 'my-pins') setMarkers(data.data.user.my_pins)
+                else setMarkers(data.data.user.all_pins)
+            }
+        })
+    }, [id])
 
 	return (
 		<>
