a28f6b9676f70b780936525158fe2247d16b995d
seunkoko <seunowonikoko@gmail.com>
2021-08-01 17:16:48 +0300

    Authenticate route and sidebar display

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -6,7 +6,10 @@ import { FiLogIn, FiLogOut, FiUser } from 'react-icons/fi';
 
 import './Sidebar.scss';
 
+import { getToken, logout } from '../../../utils/helper';
+
 const Sidebar = ({ history, collapsed, toggled, handleToggleSidebar }) => {
+	const authToken = getToken();
 
 	return (
 		<ProSidebar collapsed={collapsed} toggled={toggled} breakPoint="lg" onToggle={handleToggleSidebar}>
@@ -16,36 +19,42 @@ const Sidebar = ({ history, collapsed, toggled, handleToggleSidebar }) => {
 
 			<SidebarContent>
 				<Menu iconShape="square">
-						<>
-							<MenuItem icon={<GiPositionMarker />}>
-								All Pins
-								<Link to="/" />
-							</MenuItem>
-							<MenuItem icon={<GiPositionMarker />}>
-								My Pins
-								<Link to="/my-pins" />
-							</MenuItem>
-							<MenuItem icon={<GiPositionMarker />}>
-								Shared Pins
-								<Link to="/shared" />
-							</MenuItem>{' '}
-							<MenuItem
-								icon={<FiLogOut />}
-								onClick={() => {
-									console.log('logout')
-								}}
-							>
-								Logout
-							</MenuItem>
-							<MenuItem icon={<FiLogIn />}>
-								Login
-								<Link to="/login" />
-							</MenuItem>
-							<MenuItem icon={<FiUser />}>
-								SignUp
-								<Link to="/signup" />
-							</MenuItem>
-						</>
+						{authToken ? (
+							<>
+								<MenuItem icon={<GiPositionMarker />}>
+									All Pins
+									<Link to="/" />
+								</MenuItem>
+								<MenuItem icon={<GiPositionMarker />}>
+									My Pins
+									<Link to="/my-pins" />
+								</MenuItem>
+								<MenuItem icon={<GiPositionMarker />}>
+									Shared Pins
+									<Link to="/shared" />
+								</MenuItem>{' '}
+								<MenuItem
+									icon={<FiLogOut />}
+									onClick={() => {
+										logout();
+										history.push("/login")
+									}}
+								>
+									Logout
+								</MenuItem>
+							</>
+						) : (
+							<>
+								<MenuItem icon={<FiLogIn />}>
+									Login
+									<Link to="/login" />
+								</MenuItem>
+								<MenuItem icon={<FiUser />}>
+									SignUp
+									<Link to="/signup" />
+								</MenuItem>
+							</> 
+						)}
 				</Menu>
 			</SidebarContent>
 		</ProSidebar>
